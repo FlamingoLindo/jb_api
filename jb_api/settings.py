70826a2129ca6c35081cf3ef6aaa8d243f967ccb
Vitor <vitorantunes2003@gmail.com>
2025-04-15 00:46:58 -0300

    Refactor code structure for improved readability and maintainability

--- jb_api/settings.py
+++ jb_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -30,7 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 MEDIA_URL = '/Images/'
-MEDIA_ROOT = BASE_DIR / 'Images'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'Images')
 
 # Application definition
 
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jb'
+    'jb',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # Your Next.js development URL
+    # add production URL if necessary
 ]
 
 ROOT_URLCONF = 'jb_api.urls'
